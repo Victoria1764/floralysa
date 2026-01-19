5cce7887832e9b573c42bd0308a57c2a11247f61
Victoria1764 <vbonaert@iutbayonne.univ-pau.fr>
2026-01-19 02:12:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -199,7 +199,7 @@ function updateDisplay() {
     if (plantData && document.getElementById('plant-name')) {
         const img = document.getElementById('plant-img');
         if (img) {
-            img.src = `${plantId.toLowerCase()}.jpg`;
+            img.src = `${plantId.toLowerCase()}.png`;
             img.alt = plantData.nom;
         }
         document.getElementById('plant-name').textContent = "1. Nom : " + plantData.nom;
@@ -344,6 +344,7 @@ function updateDisplay() {
     }
 
 });
+
 
 
 
