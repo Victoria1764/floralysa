1aa4efcf7f48855c60534631e9cd931ca5fa54b3
Victoria1764 <vbonaert@iutbayonne.univ-pau.fr>
2026-01-19 01:45:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -169,7 +169,7 @@ function updateDisplay() {
                 card.className = "plant-card";
                 card.innerHTML = `
                     <figure>
-                        <img src="images/${id.toLowerCase()}.jpg" alt="${p.nom}" onerror="this.src='logo1.png'">
+                        <img src=${id.toLowerCase()}.jpg" alt="${p.nom}" onerror="this.src='logo1.png'">
                         <figcaption><h4>${p.nom}</h4></figcaption>
                     </figure>
                 `;
@@ -344,3 +344,4 @@ function updateDisplay() {
     }
 
 });
+
